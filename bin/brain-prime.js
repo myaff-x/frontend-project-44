1260085e00ae9f8d37a6c0e66f8556a83f45005e
Misha <xmyaff@gmail.com>
2026-04-17 12:52:00 +0000

    Простота чисел

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+console.log('brain-prime');
+import { runPrimeGame } from "../src/games/prime.js";
+runPrimeGame();
---
+++ src/games/prime.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+import { getRandomNumber } from '../utils.js';
+
+const checkPrime = (number) => {
+    if (number < 2 || number % 2 === 0) {return false}
+    if (number === 2) {return true}
+    const sqrtNumber = Math.sqrt(number);
+    for(let i = 3;i <= sqrtNumber;i += 2) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+export const runPrimeGame = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+    let number = getRandomNumber(0,100);
+    let correctAnswer = checkPrime(number)
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === correctAnswer) {
+        console.log('Correct!');
+        console.log(`Congratulations, ${name}`);
+    }
+    else {
+        console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
+        console.log(`Let's try again, ${name}`);
+    }
+} 
